7e81619e0e9cf6aa0883aee0bee7cf7749f4f28b
MondoBiscuit <bradleofabian@gmail.com>
2018-07-20 10:54:57 -0400

    movie data coming back

--- liri.js
+++ liri.js
@@ -6,6 +6,8 @@ var Spotify = require('node-spotify-api');
 var request = require('request');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var fs = require('fs');
+//stored info vars//
 var command = process.argv[2]
 var nodeArgv = process.argv;
 var x = "";
@@ -85,5 +87,56 @@ function spotifySong(song){
     } else{
       console.log('Error occurred.');
     }
+  });
+}
+function movie(movieData){
+  var omdbURL = 'http://www.omdbapi.com/?t=' + movieData + '&plot=short&tomatoes=true';
+
+  request(omdbURL, function (error, response, body){
+    if(!error && response.statusCode == 200){
+      var body = JSON.parse(body);
+
+      console.log("Title: " + body.Title);
+      console.log("Release Year: " + body.Year);
+      console.log("IMdB Rating: " + body.imdbRating);
+      console.log("Country: " + body.Country);
+      console.log("Language: " + body.Language);
+      console.log("Plot: " + body.Plot);
+      console.log("Actors: " + body.Actors);
+      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+      //adds text to log.txt
+      fs.appendFile('log.txt', "Title: " + body.Title);
+      fs.appendFile('log.txt', "Release Year: " + body.Year);
+      fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
+      fs.appendFile('log.txt', "Country: " + body.Country);
+      fs.appendFile('log.txt', "Language: " + body.Language);
+      fs.appendFile('log.txt', "Plot: " + body.Plot);
+      fs.appendFile('log.txt', "Actors: " + body.Actors);
+      fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
+      fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
+
+    } else{
+      console.log('Error occurred.')
+    }
+    if(movieData === "Mr. Nobody"){
+      console.log("-----------------------");
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      console.log("It's on Netflix!");
+
+      //adds text to log.txt
+      fs.appendFile('log.txt', "-----------------------");
+      fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      fs.appendFile('log.txt', "It's on Netflix!");
+    }
+  });
+}
+
+function sayWhat(){
+  fs.readFile('random.txt', "utf8", function(error, data){
+    var txt = data.split(',');
+
+    spotifySong(txt[1]);
   });
 }
